b9306a1ca1b108c5bb86f1b5a86a1b54c6f0272f
sanyog249 <sanyogp.249@gmail.com>
2025-03-10 22:56:23 +0530

    listing logic fixed

--- src/v1_modules/folder_managment/services.py
+++ src/v1_modules/folder_managment/services.py
@@ -349,7 +349,7 @@ class FolderService:
                                             user_files.append(file_response)
 
                 if user_folders or user_files:
-                    user_resources[f"{user.username} (ID: {user.id})"] = {
+                    user_resources[user.username]  = {
                         "folders": [FolderResponse.from_orm(folder) for folder in user_folders],
                         "files": [FileResponse.from_orm(file) for file in user_files]
                     }
